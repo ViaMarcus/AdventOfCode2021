f53ba7646b3f5fd12a7ab04780bc00156bfcf572
Marcus <marcus.sjoqvist@viametrics.com>
2021-12-10 12:48:23 +0100

    complete day 10

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    Day5.solveDay5Part2()
+    // Day5.solveDay5Part2()
     // Day8.solveDay8Part2()
     // Day9.solveDay9Part2()
+    Day10.solveDay10Part2()
 
 }
 
